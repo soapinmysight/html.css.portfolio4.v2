eb0c5ddd10e34ed7e1c1b14704b8e9bf048be0b0
soapinmysight <dieuwvr@gmail.com>
2023-05-25 15:30:34 +0200

    json is working and show detail button is added

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,33 +1,55 @@
 window.addEventListener('load',init)
 
+// Function to initialize the code
 function init() {
+    // Call the 'getJSONdata' function with the specified API URL
     getJSONdata(`webservice/index.php`, displayCards)
 }
 
 function getJSONdata(apiUrl, successHandler)
 {
+    // Fetch data from the API URL
     fetch(apiUrl)
         .then((response) => {
+            // Check if the response is not okay (aka not successful)
             if (!response.ok) {
+                // if not okay, throw an error
                 throw new Error(response.statusText);
             }
+            // Parse the response as JSON and return the result
             return response.json();
         })
         .then(successHandler)
         .catch(ajaxErrorHandler);
 }
 
+// Function to display the cards using the retrieved data
  function displayCards(data){
-    for (let i = 1; i < data.length+1; i++){
+    //for loop so we loop through the numbers we need to load the data in
+    for (let i = 0; i < data.length+1; i++){
+        // Get the element with the ID corresponding to the current index
+        //(aka the number from the for loop)
         let cards = document.getElementById(`${i}`)
 
         //console logs for debugging purposes
         console.log(`${i}`)
         console.log(data[i].title)
 
+        // Create an 'h3' element for the title
         let title = document.createElement("h3")
+        // Set the inner HTML of the title element (which we just created)
+        // to the title value from the data
         title.innerHTML = data[i].title
+        // Append the title element to the 'cards' element
         cards.appendChild(title)
+
+        //next piece of code creates a detail button for each part so we can show the description of each part
+        //create a new HTML button element and assigns it to the variable button
+        let button = document.createElement('button');
+        // Set the inner HTML of the button element (which we just created)
+        button.innerHTML = 'Show Details';
+        button.dataset.id = data[i];
+        cards.appendChild(button);
     }
  }
 
--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -6,27 +6,27 @@ function getCards()
 {
     return [
         [
-            "id" => 1,
+            "id" => 0,
             "title" => "Digitaal",
         ],
         [
-            "id" => 2,
+            "id" => 1,
             "title" => "Schilderijtjes",
         ],
         [
-            "id" => 3,
+            "id" => 2,
             "title" => "Kastje",
         ],
         [
-            "id" => 4,
+            "id" => 3,
             "title" => "CareBot",
         ],
         [
-            "id" => 5,
+            "id" => 4,
             "title" => "Schermvereniging En Garde<span> Reservatie Website</span>",
         ],
         [
-            "id" => 6,
+            "id" => 5,
             "title" => "Website met hulpmiddelen voor mensen met een <span>auditieve beperking</span>",
         ]
     ];
@@ -39,24 +39,24 @@ function getCards()
 function getCardsDetails($id)
 {
     $tags = [
-        1 => [
+        0 => [
             "description" => "Hier zijn twee digitale tekeningen.",
         ],
-        2 => [
+        1 => [
             "description" => "4 schilderijtje",
         ],
-        3 => [
+        2 => [
             "description" => "Dit kastje heb ik gemaakt omdat ik wou kunnen lassen (en ook omdat ik een kastje wou hebben)",
         ],
-        4 => [
+        3 => [
             "description" => "We Care So You Don't Have To. De CareBot helpt jou met je dagelijkse pillen innemen en daar water bij te drinken. Zo verspil je in de ochtend niet tijd aan je pillen bij elkaar zoeken! Er is zelfs de mogelijkheid voor pillen die je niet een keer per dag neemt.",
         ],
-        5 => [
-            "description" => "In dit project maakte ik een reservatie systeem voor de verhuuring van schremproducten voor de scherm vereniging En Garde. Dit systeem werkte met 3 databases, en ik gebruikte sql, php, html en css. Mijn code kan je <a href=https://github.com/soapinmysight/reserveringssysteem_engarde target="_blank">hier</a> terug vinden, en een video met uitleg over de functionaliteiten van de website vind je <a href=https://www.youtube.com/watch?v=KLmgvSJifv0 target="_blank"> hier</a>.",
+        4 => [
+            "description" => "In dit project maakte ik een reservatie systeem voor de verhuuring van schremproducten voor de scherm vereniging En Garde. Dit systeem werkte met 3 databases, en ik gebruikte sql, php, html en css. Mijn code kan je hier: https://github.com/soapinmysight/reserveringssysteem_engarde terug vinden, en een video met uitleg over de functionaliteiten van de website vind je hier: https://www.youtube.com/watch?v=KLmgvSJifv0.",
         ],
-        6 => [
+        5 => [
             "description" => "Uit onderzoek blijkt dat mensen met een auditieve beperking minder sporten dan mensen zonder een auditieve beperking. Dat een auditieve beperking mensen zo belemmerd, kan echt niet meer. Sporten moet toegankelijker worden voor mensen met een auditieve beperking. Daarom maakte wij (ik en de andere leden in team 23) een website, met 4 webpages, die precies dat als doel hebben.
-                Onze code kan je <a href=https://github.com/RadiazOm/CLE3 target="_blank"> hier </a> terug vinden. De bovenste foto hieronder, is de gazamelijke homepage. Daaronder zie je mijn eigen pagina; een notitie pagina.",
+                Onze code kan je hier: https://github.com/RadiazOm/CLE3 terug vinden. De bovenste foto hieronder, is de gazamelijke homepage. Daaronder zie je mijn eigen pagina; een notitie pagina.",
         ],
     ];
 
