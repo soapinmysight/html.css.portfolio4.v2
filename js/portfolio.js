c0b9ece87f3a4a6adf3ace8227b98d14741334bf
soapinmysight <dieuwvr@gmail.com>
2023-05-25 22:28:03 +0200

    i try to add modal (also added alt for pictures)

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,8 @@
-window.addEventListener('load',init)
+window.addEventListener('load', init)
 
 //Global variables
 let portfolio;
+let card;
 
 // Function to initialize the code
 function init() {
@@ -12,8 +13,7 @@ function init() {
     getJSONdata(`webservice/index.php`, displayCards)
 }
 
-function getJSONdata(apiUrl, successHandler)
-{
+function getJSONdata(apiUrl, successHandler) {
     // Fetch data from the API URL
     fetch(apiUrl)
         .then((response) => {
@@ -30,16 +30,12 @@ function getJSONdata(apiUrl, successHandler)
 }
 
 // Function to display the cards using the retrieved data
- function displayCards(data){
-    //for loop so we loop through the numbers we need to load the data in
-    for (let i = 0; i < data.length+1; i++){
+function displayCards(data) {
+    //for loop to we loop through the numbers we need to load the data in
+    for (let i = 0; i < data.length + 1; i++) {
         // Get the element with the ID corresponding to the current index
         //(aka the number from the for loop)
         let cards = document.getElementById(`${i}`)
-
-        //console logs for debugging purposes
-        console.log(`${i}`)
-        console.log(data[i].title)
 
         // Create an 'h3' element for the title
         let title = document.createElement("h3")
@@ -57,11 +53,10 @@ function getJSONdata(apiUrl, successHandler)
         button.dataset.id = data[i];
         cards.appendChild(button);
     }
- }
+}
 
 //This function will handle click events
-function portfolioClickHandler(e)
-{
+function portfolioClickHandler(e) {
 //This line assigns the target of the event (e.target) to the variable clickedItem. The target represents the element that triggered the event, in this case, the element that was clicked
     let clickedItem = e.target;
 //This line checks if the nodeName property of the clickedItem is not equal to the string 'BUTTON'.
@@ -72,33 +67,43 @@ function portfolioClickHandler(e)
         return;
     }
     displayDetail()
+    console.log("displaydetail")
 }
 
-function displayDetail(e){
-    // //Get the information from the global stored data
-    // let pokemon = pokemonData[clickedItem.dataset.id];
-    //
-    // //Reset the content
-    // detailContent.innerHTML = '';
-    //
-    // //Show the name we used on the main grid
-    // let title = document.createElement('h1');
-    // title.innerHTML = `${pokemon.name} (#${pokemon.id})`;
-    // detailContent.appendChild(title);
-    //
-    // //Display the shiny
-    // let shiny = document.createElement('img');
-    // shiny.src = pokemon.sprites.other.home.front_shiny;
-    // detailContent.appendChild(shiny);
-    //
-    // //Open the modal
-    // detailDialog.showModal();
-    // gallery.classList.add('dialog-open');
-}
+function displayDetail(data) {
+    //for loop so we loop through the numbers we need to load the data in
+    for (let i = 0; i < data.length + 1; i++) {
+        // Get the element with the ID corresponding to the current index
+        //(aka the number from the for loop)
+        let cards = document.getElementById(`${i}`)
+
+        // Create an 'h3' element for the title
+        let title = document.createElement("h3")
+        // Set the inner HTML of the title element (which we just created)
+        // to the title value from the data
+        title.innerHTML = data[i].title
+        // Append the title element to the 'cards' element
+        cards.appendChild(title)
+
+        //show the detail description
+        let description = document.createElement('p')
+        description.innerHTML = data[i].description;
+        card.appendChild(description);
+
+        //create close button
+        let button = document.createElement('button');
+        // Set the inner HTML of the button element (which we just created)
+        button.innerHTML = 'close';
+        cards.appendChild(button);
+
+        // Open the modal (detail dialog)
+        detailDialog.showModal();
+
+        // Add the 'dialog-open' class to the gallery element to apply a blur effect
+        portfolio.classList.add('dialog-open');
+    }}
 
 
-
-
- function ajaxErrorHandler(e){
-    console.log(e)
- }
+    function ajaxErrorHandler(e) {
+        console.log(e)
+    }
