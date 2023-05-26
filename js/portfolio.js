f62de6f76f48907d7239acfe4ab7d8bab1f6b442
soapinmysight <dieuwvr@gmail.com>
2023-05-26 15:56:34 +0200

    alles is af alleen nog even netheid dingetjes en comments!!!!! en presentatie strctuur enzo!!!!!!!

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,14 +3,23 @@ window.addEventListener('load', init)
 //Global variables
 let portfolio;
 let card;
+let detailDialog;
+let detailContent;
+let favItems = [];
 
 // Function to initialize the code
 function init() {
     // Retrieve the portfolio element and add a click event listener
     portfolio = document.getElementById('projects');
     portfolio.addEventListener('click', portfolioClickHandler);
+
     // Call the 'getJSONdata' function with the specified API URL
     getJSONdata(`webservice/index.php`, displayCards)
+
+    detailDialog = document.getElementById('description');
+    detailContent = detailDialog.querySelector('.modal-content');
+    detailDialog.addEventListener('click', detailModalClickHandler);
+    detailDialog.addEventListener('close', dialogCloseHandler);
 }
 
 function getJSONdata(apiUrl, successHandler) {
@@ -32,7 +41,7 @@ function getJSONdata(apiUrl, successHandler) {
 // Function to display the cards using the retrieved data
 function displayCards(data) {
     //for loop to we loop through the numbers we need to load the data in
-    for (let i = 0; i < data.length + 1; i++) {
+    for (let i = 0; i < data.length; i++) {
         // Get the element with the ID corresponding to the current index
         //(aka the number from the for loop)
         let cards = document.getElementById(`${i}`)
@@ -50,14 +59,33 @@ function displayCards(data) {
         let button = document.createElement('button');
         // Set the inner HTML of the button element (which we just created)
         button.innerHTML = 'Show Details';
-        button.dataset.id = data[i];
+        //geef id mee bij het klikken op de button
+        button.dataset.id = data[i].id;
         cards.appendChild(button);
+
+        //next piece of code creates a detail button for each part so we can Add Favorite
+        //create a new HTML button element and assigns it to the variable button
+        let buttonFav = document.createElement('button');
+        // Set the inner HTML of the button element (which we just created)
+        buttonFav.innerHTML = 'Add Favorite';
+        //geef id mee bij het klikken op de button
+        buttonFav.dataset.id = data[i].id;
+        buttonFav.id = `fav-button${i}`;
+        cards.appendChild(buttonFav);
+    }
+    let favoriteItemString = localStorage.getItem('favItems')
+    if (favoriteItemString !== null){
+        favItems = JSON.parse(favoriteItemString)
+        for (let favoriteItem of favItems){
+            addFavorite(favoriteItem)
+        }
     }
 }
 
 //This function will handle click events
 function portfolioClickHandler(e) {
-//This line assigns the target of the event (e.target) to the variable clickedItem. The target represents the element that triggered the event, in this case, the element that was clicked
+//This line assigns the target of the event (e.target) to the variable clickedItem.
+// The target represents the element that triggered the event, in this case, the element that was clicked
     let clickedItem = e.target;
 //This line checks if the nodeName property of the clickedItem is not equal to the string 'BUTTON'.
 //The nodeName property represents the name of the node (HTML element) in uppercase.
@@ -66,42 +94,102 @@ function portfolioClickHandler(e) {
     if (clickedItem.nodeName !== 'BUTTON') {
         return;
     }
-    displayDetail()
-    console.log("displaydetail")
+    //assigning id to the variable card
+    card = clickedItem.dataset.id;
+    //
+    if (clickedItem.innerHTML === 'Show Details') {
+    getJSONdata(`webservice/index.php?id=${card}`, displayDetail)
+    }
+    //
+    else if  (clickedItem.innerHTML === 'Add Favorite'){
+        addFavorite(card)
+        addItemLocalStorage(card)
+    }
+    else if (clickedItem.innerHTML === 'Remove Favorite'){
+        removeFavorite(card)
+        removeItemLocalStorage(card)
+    }
+
+
 }
 
 function displayDetail(data) {
-    //for loop so we loop through the numbers we need to load the data in
-    for (let i = 0; i < data.length + 1; i++) {
+    console.log(data)
+    detailContent.innerHTML = '';
         // Get the element with the ID corresponding to the current index
         //(aka the number from the for loop)
-        let cards = document.getElementById(`${i}`)
+        let cards = document.getElementById(`${card}`)
 
         // Create an 'h3' element for the title
         let title = document.createElement("h3")
         // Set the inner HTML of the title element (which we just created)
         // to the title value from the data
-        title.innerHTML = data[i].title
+        title.innerHTML = data.title;
         // Append the title element to the 'cards' element
-        cards.appendChild(title)
+    detailContent.appendChild(title)
 
         //show the detail description
         let description = document.createElement('p')
-        description.innerHTML = data[i].description;
-        card.appendChild(description);
+        description.innerHTML = data.description;
+    detailContent.appendChild(description);
 
         //create close button
         let button = document.createElement('button');
         // Set the inner HTML of the button element (which we just created)
         button.innerHTML = 'close';
-        cards.appendChild(button);
+    detailContent.appendChild(button);
 
         // Open the modal (detail dialog)
         detailDialog.showModal();
 
         // Add the 'dialog-open' class to the gallery element to apply a blur effect
         portfolio.classList.add('dialog-open');
-    }}
+    }
+
+/**
+ * Close the modal if clicked on the close button or outside the modal (on the backdrop)
+ *
+ * @param e
+ */
+function detailModalClickHandler(e)
+{
+    if (e.target.nodeName === 'DIALOG' || e.target.nodeName === 'BUTTON') {
+        detailDialog.close();
+    }
+}
+
+/**
+ * Close the underlying blur effect when dialog is closed (both on our own click or the native ESC key)
+ *
+ * @param e
+ */
+//TODO remove this or use this
+function dialogCloseHandler(e)
+{
+    portfolio.classList.remove('dialog-open');
+}
+
+//function for changing colors
+function addFavorite(card){
+    let buttonFav = document.getElementById(`fav-button${card}`)
+    buttonFav.innerHTML = 'Remove Favorite'
+}
+function addItemLocalStorage(card){
+    console.log(card)
+    favItems.push(card)
+    localStorage.setItem("favItems", JSON.stringify(favItems))
+    console.log(localStorage.getItem("favItems"))
+}
+
+function removeFavorite(card){
+    let buttonFav = document.getElementById(`fav-button${card}`)
+    buttonFav.innerHTML = 'Add Favorite'
+}
+function removeItemLocalStorage(card){
+    let itemPosition = favItems.indexOf(card)
+    favItems.splice(itemPosition, 1)
+    localStorage.setItem("favItems", JSON.stringify(favItems))
+}
 
 
     function ajaxErrorHandler(e) {
--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -23,11 +23,11 @@ function getCards()
         ],
         [
             "id" => 4,
-            "title" => "Schermvereniging En Garde<span> Reservatie Website</span>",
+            "title" => "Schermvereniging En Garde Reservatie Website",
         ],
         [
             "id" => 5,
-            "title" => "Website met hulpmiddelen voor mensen met een <span>auditieve beperking</span>",
+            "title" => "Website met hulpmiddelen voor mensen met een auditieve beperking",
         ]
     ];
 }
@@ -40,21 +40,27 @@ function getCardsDetails($id)
 {
     $tags = [
         0 => [
+            "title" => "Digitaal",
             "description" => "Hier zijn twee digitale tekeningen.",
         ],
         1 => [
+            "title" => "Schilderijtjes",
             "description" => "4 schilderijtje",
         ],
         2 => [
+            "title" => "Kastje",
             "description" => "Dit kastje heb ik gemaakt omdat ik wou kunnen lassen (en ook omdat ik een kastje wou hebben)",
         ],
         3 => [
+            "title" => "CareBot",
             "description" => "We Care So You Don't Have To. De CareBot helpt jou met je dagelijkse pillen innemen en daar water bij te drinken. Zo verspil je in de ochtend niet tijd aan je pillen bij elkaar zoeken! Er is zelfs de mogelijkheid voor pillen die je niet een keer per dag neemt.",
         ],
         4 => [
+            "title" => "Schermvereniging En Garde Reservatie Website",
             "description" => "In dit project maakte ik een reservatie systeem voor de verhuuring van schremproducten voor de scherm vereniging En Garde. Dit systeem werkte met 3 databases, en ik gebruikte sql, php, html en css. Mijn code kan je hier: https://github.com/soapinmysight/reserveringssysteem_engarde terug vinden, en een video met uitleg over de functionaliteiten van de website vind je hier: https://www.youtube.com/watch?v=KLmgvSJifv0.",
         ],
         5 => [
+            "title" => "Website met hulpmiddelen voor mensen met een auditieve beperking",
             "description" => "Uit onderzoek blijkt dat mensen met een auditieve beperking minder sporten dan mensen zonder een auditieve beperking. Dat een auditieve beperking mensen zo belemmerd, kan echt niet meer. Sporten moet toegankelijker worden voor mensen met een auditieve beperking. Daarom maakte wij (ik en de andere leden in team 23) een website, met 4 webpages, die precies dat als doel hebben.
                 Onze code kan je hier: https://github.com/RadiazOm/CLE3 terug vinden. De bovenste foto hieronder, is de gazamelijke homepage. Daaronder zie je mijn eigen pagina; een notitie pagina.",
         ],
